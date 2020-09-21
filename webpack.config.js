a606432496831c12e3a9a70d1988349c44837513
Nhung Hang <hangbichnhung@gmail.com>
2020-09-22 01:44:00 +0700

    fix webpack config to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,9 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "/dist"),
+    path: path.join(__dirname, "/dist"),
     filename: "bundle.js",
-    // publicPath: "./src/assets",
+    publicPath: "/",
   },
 
   module: {
@@ -20,27 +20,31 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"],
+        test: /\.(svg|png)$/,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
       },
     ],
   },
 
   devServer: {
-    contentBase: path.resolve(__dirname, "dist"),
+    contentBase: path.resolve(__dirname, "/dist"),
     port: 3001,
   },
 
-  resolve: {
-    extensions: [".mjs", ".ts", ".tsx", ".js", ".jsx"],
-    alias: {
-      // Here is some example for aliases. now you can use absolute import.
-      "@components": path.resolve(__dirname, "./src/components"),
-    },
-  },
+  // resolve: {
+  //   extensions: [".mjs", ".ts", ".tsx", ".js", ".jsx"],
+  //   alias: {
+  //     // Here is some example for aliases. now you can use absolute import.
+  //     "@components": path.resolve(__dirname, "./src/components"),
+  //   },
+  // },
 
   plugins: [
     new CleanWebpackPlugin(),
